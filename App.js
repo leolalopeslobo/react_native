fc0c7fdfaefcb72b6f9747815864feec672275eb
leolalopeslobo <83197830+leolalopeslobo@users.noreply.github.com>
2023-01-20 15:59:28 +0530

    first code

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>REACT NATIVE!!</Text>
+      </View>
+      <Text>This is my First App</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: '#fff',
+    backgroundColor: "#02a8d0",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  }
+});
