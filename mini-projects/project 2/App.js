3a0c015dcb9b41afb7311b8b497b37df13fb3339
leolalopeslobo <83197830+leolalopeslobo@users.noreply.github.com>
2023-01-31 13:16:02 +0530

    updating ToDo App with Flexbox

--- mini-projects/project 2/App.js
+++ mini-projects/project 2/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyb
 import Header from './components/header';
 import TodoItem from './components/todoitem';
 import AddTodo from './components/addtodo';
+import Sandbox from './components/sandbox';
 
 export default function App() {
     //creating the state using a hook
@@ -38,6 +39,7 @@ export default function App() {
     }
 
   return (
+    // <Sandbox />
     <TouchableWithoutFeedback onPress={() => {
       Keyboard.dismiss();
       console.log('dismissed keyboard')
@@ -67,9 +69,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   content: {
+    flex: 1,
     padding: 40,
   },
   list: {
+    flex: 1,
     marginTop: 20,
   }
 });
